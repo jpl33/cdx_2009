6ecbde4159bf003fbcf8d747f36b7bc50d50acf1
jpl33 <pladott1@gmail.com>
2017-05-04 02:42:19 +0300

    start taking over view of snort alerts

--- capinfos_pcap_dirs.py
+++ capinfos_pcap_dirs.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 import pathlib
 import pathlib
+import pandas as pd
 
 from subprocess import Popen, PIPE
 from os import path
@@ -14,8 +15,12 @@ import logging
 
 
 
-pcap_dir = "/mnt/hgfs/cdx_2009/sandbox_win"
+#pcap_dir = "/mnt/hgfs/cdx_2009/sandbox_win"
+home_dir='D:\\personal\\msc\\maccdc_2012\\'
+pcap_dir= 'maccdc2012_00003\\'
 pcap=(".pcap",".dmp",",pcapng")
+conn_frmt=['ts','uid','id.orig_h','id.orig_p','id.resp_h','id.resp_p','proto','service','duration','orig_bytes','resp_bytes','conn_state','local_orig','local_resp','missed_bytes','history','orig_pkts','orig_ip_bytes','resp_pkts','resp_ip_bytes','tunnel_parents','orig_cc','resp_cc','sensorname']
+snrt_frmt=['timestamp','sig_generator','sig_id','sig_rev','msg','proto','src','srcport','dst','dstport','ethsrc','ethdst','ethlen','tcpflags','tcpseq','tcpack','tcplen','tcpwindow','ttl','tos','id','dgmlen','iplen','icmptype','icmpcode','icmpid','icmpseq']
 
 def is_pcap(file_name):
     """simple function, user modifiable, to determine if a given file is in fact a PCAP file we want to process
@@ -41,11 +46,12 @@ def main():
     
     
     # Get list of PCAP files to ready to be processed - if ctime of file is less than ctime of next file in file ring buffer
-    print ("Pcap files dir %s" %pcap_dir)    
-    pcap_file_list = os.listdir(pcap_dir)
+    print ("Pcap files dir %s" %home_dir)    
+    pcap_file_list = os.listdir(home_dir)
     pcap_file_list.sort()
     print ("- files found %s"%pcap_file_list)
-    out_file= open('pcap_info.txt', 'r+') 
+    out_file= open('pcap_info.txt', 'r+')
+    prcf_file= open('processed_files.txt', 'r+')
     is_header(out_file)
     # go through list and remove entries that aren't files we care about
     to_be_removed = []
@@ -56,10 +62,10 @@ def main():
     for f in pcap_file_list:
         if not is_pcap(f):
             to_be_removed.append(f)
-#    for l in out_file.readlines():
-#        l=l.split("\n")[0]
-#        to_be_removed.append(l)
-#    
+    for l in prcf_file.readlines():
+        l=l.split("\n")[0]
+        to_be_removed.append(l)
+    
     
     for f in to_be_removed:
         print ("Not pcap so not processing %s"%f)
@@ -72,26 +78,44 @@ def main():
       
     print ("NUMBER OF FILES IN FOLDER")
     print (len(pcap_file_list))
-      
+    alrt_sum=pd.DataFrame()   
+    nmm=[]
     sdpwd="S3cur!ty"
     print ("running snort ")
     for pf in pcap_file_list:
          print ("current file : %s" % pf)
         # since snort takes so long to start up, only run it once at the end on all the pcap files at once
-         output_dirname =pf.split(".")[0]
-         pcap_snort=(pcap_dir+"/"+output_dirname+"/" + pf)         
+         pcap_name =pf.split(".")[0]
+         pcap_snort=(home_dir + pf)         
 
          #is__dir(pcap_dir,output_dirname)
          if (hdr==0):
-             capinfos_cmd = "sudo capinfos -m -a -e -r -T %s" %(pcap_snort)
+            # capinfos_cmd = "sudo capinfos -m -a -e -r -T %s" %(pcap_snort)
+            win_cmd="capinfos -m -a -e -r -T %s" %(pcap_snort)
          else:
-             capinfos_cmd = "sudo capinfos -m -a -e -T %s" %(pcap_snort)
+             #capinfos_cmd = "sudo capinfos -m -a -e -T %s" %(pcap_snort)
+             win_cmd="capinfos -m -a -e -T %s" %(pcap_snort)
              
-         sn=capinfos_cmd.split()
+         #sn=capinfos_cmd.split()
+         
+         wn_cmd=win_cmd.split()
          #run_capinfos = subprocess.Popen(['sudo', '-S']+sn, stdin=PIPE, stderr=PIPE,universal_newlines=True)
-         run_capinfos = subprocess.check_output(['sudo', '-S']+sn, stdin=PIPE, stderr=PIPE,universal_newlines=True)
-       
-         out_file.write(run_capinfos)
+         #run_capinfos = subprocess.check_output(['sudo', '-S']+sn, stdin=PIPE, stderr=PIPE,universal_newlines=True)
+         
+         run_win_cmd = subprocess.check_output(wn_cmd, stdin=PIPE, stderr=PIPE,universal_newlines=True)
+         
+         df = pd.read_csv(home_dir+pcap_dir +'conn.log',sep='\t',comment='#',names=conn_frmt)
+         srvc=list(df['service'].unique())
+         srvc_nm=df['service'].value_counts()
+         alrt = pd.read_csv(home_dir+pcap_dir +'alert.csv',sep=',',comment='#',names=snrt_frmt)
+         alrt_nm=alrt['msg'].value_counts()
+         alrt_sig=alrt['sig_id'].value_counts()
+         
+         out_file.write(run_win_cmd)
+         out_file.write(srvc_nm.to_string())
+         out_file.write(alrt_nm.to_string())
+         out_file.write(alrt_sig.to_string())
+
          out_file.flush()
 
 
--- merge_bro_json.py
+++ merge_bro_json.py
@@ -42,14 +42,15 @@ def mongo_json(mydict):
            if key in mongo_fields:
                mydict[mongo_fields[key]]= mydict.pop(key)
                
-#def insert_to_mongo(file_name, collection):
-    #file_name.close()
-    #with open(home_dir+pcap_dir +file,'r') as file_name:
-#    #for line in itertools.islice(file_name, 0,5):
-#    for line in file_name.readlines():
-#       jsndict=json.loads(line)
-#       mongo_json(jsndict)
-#       collection.insert(jsndict)
+def insert_to_mongo(file_name, collection):
+    file_name.close()
+    with open(home_dir+pcap_dir +file_name,'r') as file_name:
+    #for line in itertools.islice(file_name, 0,5):
+        for line in file_name.readlines():
+           jsndict=json.loads(line)
+           mongo_json(jsndict)
+           collection.insert(jsndict)
+
 def  mongo_trim(myDict):
       lmng=list(mongo_fields.keys())
       for ll in lmng:
@@ -78,7 +79,7 @@ query=[ntlm_data[0]['id.orig_h'],ntlm_data[0]['id.orig_p'],ntlm_data[0]['id.resp
 
 nt_json=[]
 import ijson
-#fconn = open(home_dir+pcap_dir +'conn2.json', 'rb')
+#fconn = open(home_dir+pcap_dir +'conn2.json', 'r')
 
 #for it in ijson.items(fconn, 'item'):
 #    if ('ntlm' in it['service']):
@@ -91,7 +92,13 @@ import ijson
 #            print(it)
 #       print(nt['id.orig_h'])
 
-#df = DataFrame.from_csv(home_dir+pcap_dir +'ntlm.txt', sep="\t")        
+df = pd.read_csv(home_dir+pcap_dir +'conn.log',sep='\t',comment='#',names=['ts','uid','id.orig_h','id.orig_p','id.resp_h','id.resp_p','proto','service','duration','orig_bytes','resp_bytes','conn_state','local_orig','local_resp','missed_bytes','history','orig_pkts','orig_ip_bytes','resp_pkts','resp_ip_bytes','tunnel_parents','orig_cc','resp_cc','sensorname'])
+srvc=list(df['service'].unique())
+srvc_nm=df['service'].value_counts()
+print(srvc)
+
+
+
 #df=pd.read_json(home_dir+pcap_dir +'conn.json',orient= 'records',lines=True)
 #df.columns
 #query2=df.columns
@@ -119,4 +126,4 @@ for nt in ntlm_data:
            for doc in collection.find({'uid':nt['uid']}):
                mongo_json(nt)
                nt2.append(nt)
-               collection.update_one({'_id':doc['_id']},{'$set':{'ntlm':nt2}})
+#               collection.update_one({'_id':doc['_id']},{'$set':{'ntlm':nt2}})
