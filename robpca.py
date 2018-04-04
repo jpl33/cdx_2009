2379bb1b0e94e5b1611d67e17aab456c132ab4ad
jpl33 <pladott1@gmail.com>
2018-04-04 18:15:19 +0300

    Finished? looks like it

--- robpca.py
+++ robpca.py
@@ -70,10 +70,20 @@ class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if hasattr(obj, 'to_json'):
             return obj.to_json(orient='records')
+        if isinstance(obj, bool):
+            return str(obj).lower()
         return json.JSONEncoder.default(self, obj)
 
+def json_bool(obj):
+    if isinstance(obj, bool):
+            return str(obj).lower()
+    if isinstance(obj, np.bool_):
+            return str(obj).lower()
+    return obj
 
-pcap_dir= 'maccdc2012_00001'
+
+
+pcap_dir= 'maccdc2012_00002'
 
 client = pymongo.MongoClient('localhost')
 db = client['local']
@@ -141,8 +151,8 @@ def jsd(df,mcd):
     dff['m']=dff.sum(axis=1)/2
     dff['kl_df']=dff.df*np.log(dff.df/dff.m)
     dff['kl_mcd']=dff.mcd*np.log(dff.mcd/dff.m)
-    dff.fillna(0)    
     dff['jsd']=dff.loc[:,'kl_df':'kl_mcd'].sum(axis=1)/2
+    dff.fillna(0,inplace=True)
     return dff.jsd
 
 #intervals=round(finish/interval_size)
@@ -204,6 +214,10 @@ for index in range(intervals):
     df2['cumultv_pkt_count']=0
     df['cumultv_pkt_count']=0
     
+    msg='start looking for bad flows. Line217'
+    myLogger.error(msg)
+    
+    
     gb=df2.groupby(['id_orig_h','id_resp_h'])
     ggtsdf=list()
     gdict=gb.groups
@@ -247,6 +261,8 @@ for index in range(intervals):
         outly_th=0.25*(df_cnt-outly_flws)
         if num_sum-outly_flws<outly_th:
             break
+    msg='finish looking for bad flows. Line264: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
     
     for ppn in outly_pairs:
         # # dump all flows belonging to orig-resp pairs in outly_pairs from the overall bin flows
@@ -260,6 +276,9 @@ for index in range(intervals):
     dirty_flws=list(set(df3.index.values)-set(df_clean.index.values))
     
     df_mat=df_c_n.as_matrix()
+    
+    msg='start first robpca. Line280: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
     
     rpy2.robjects.numpy2ri.activate()
     rospca=importr("rospca")
@@ -278,15 +297,18 @@ for index in range(intervals):
     OD_flag=np.array(rpca[14])
     
    
-    robust_base=importr("robustbase")
-    mcd=robust_base.covMcd(df_mat,cor = True, alpha=0.75)
-    mcd_cov=np.array(mcd[3])
-    mcd_cor=np.array(mcd[6])
+#    robust_base=importr("robustbase")
+#    mcd=robust_base.covMcd(df_mat,cor = True, alpha=0.75)
+#    mcd_cov=np.array(mcd[3])
+#    mcd_cor=np.array(mcd[6])
     
 
     # # standardized Data Frame- robust PCA center multiplied by the PCA loadings, will give us our PCA scores
     sc_3=(df3_norm.as_matrix()-center).dot(loadings)
     sd_3=pd.DataFrame()
+    
+    msg='start first OD, SD compute. Line310: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
     
     # # compute SD - Square mahalanobis Distance, for each PCA loading
     for cc in range(0,sc_3.shape[1]):
@@ -325,22 +347,27 @@ for index in range(intervals):
     df.loc[sd_3.sd_flag==0,'SD_anomaly']=True
     df['SD_feature']=False
     df.loc[sd_3.SD_feature!=0,'SD_feature']=feat_vec_sd
-    df['OD']=sd_3.sd_mine
+    df['OD']=sd_3.od_mine
     df['OD_anomaly']=False
     df.loc[sd_3.sd_flag==0,'OD_anomaly']=True
     df['OD_feature']=False
     df.loc[sd_3.OD_feature!=0,'OD_feature']=feat_vec_od
+    
+    msg='start jsd compute. Line356: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
     
 
     # # find df_clean index that was used for mcd
     mcd_index=df_clean.iloc[H1==1].index.values
     
     mcd_serv=df.loc[mcd_index,'service'].value_counts()/len(mcd_index)
+    # # high serv_jsd: big difference in probability for service between mcd and full sample
     serv_jsd=jsd(serv_freq,mcd_serv)
     for idd in serv_jsd.index.values:
         df.loc[df.service==idd,'serv_jsd']= serv_jsd.loc[idd]  
     
     mcd_history=df.loc[mcd_index,'history'].value_counts()/len(mcd_index)
+    # # high history_jsd: big difference in probability for history between mcd and full sample
     history_jsd=jsd(history_freq,mcd_history)
     for idd in history_jsd.index.values:
         df.loc[df.history==idd,'history_jsd']= history_jsd.loc[idd]  
@@ -350,67 +377,153 @@ for index in range(intervals):
     for idd in conn_state_jsd.index.values:
         df.loc[df.conn_state==idd,'conn_state_jsd']= conn_state_jsd.loc[idd]  
     
-    df32=df[df_feature_cols2]
+    
+    df.fillna(0,inplace=True)
+    df32=df[df_feature_cols2].copy()
+    df32.fillna(0,inplace=True)
     df_clean2=df32[~df32.index.isin(dirty_flws)]
+    df_c_n2=(df_clean2-df_clean2.mean() )/df_clean2.std(ddof=0)
+    df32_n=(df32-df_clean2.mean())/df_clean2.std(ddof=0)
+    df_c_n2.fillna(0,inplace=True)
+    df32_n.fillna(0,inplace=True)
+
+    msg='start second robpca. Line390: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
+    
+    rpca2=rospca.robpca(x=df_c_n2.as_matrix(),mcd=True,ndir=5000)
+    loadings2=np.array(rpca2[0])
+    e_vals2=np.array(rpca2[1])
+    scores2=np.array(rpca2[2])
+    center2=np.array(rpca2[3])
+    H02=np.array(rpca2[5])
+    H12=np.array(rpca2[6])
+    SD2=np.array(rpca2[9])
+    OD2=np.array(rpca2[10])
+    SD_th2=np.array(rpca2[11])
+    OD_th2=np.array(rpca2[12])
+    SD_flag2=np.array(rpca2[13])
+    OD_flag2=np.array(rpca2[14])
+
+    # # standardized Data Frame- robust PCA center multiplied by the PCA loadings, will give us our PCA scores
+    sc_32=(df32_n.as_matrix()-center2).dot(loadings2)
+    sd_32=pd.DataFrame()
+    
+    msg='start second OD,SD compute. Line411: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
+        
+    # # compute SD - Square mahalanobis Distance, for each PCA loading
+    for cc in range(0,sc_32.shape[1]):
+        sd_32[cc]=sc_32[:,cc]**2/e_vals2[cc]
+        
+    # # sum the SD's up and take their  sqrt() for the total SD
+    sd_3['sd_mine2']=sd_32.iloc[:,0:sc_32.shape[1]].sum(axis=1)
+    sd_3['sd_mine2']=np.sqrt(sd_3['sd_mine2'].values)
+    sd_3['sd_flag2']=1
+    sd_3.loc[sd_3.sd_mine2>SD_th2[0],'sd_flag2']=0
+    
+    # #  compute the PCA sub-space covariance matrix
+    num_e_vals2=e_vals2.shape[0]
+    lambda_mat2=e_vals2*np.identity(num_e_vals2)    
+    pca_cov2=(loadings2.dot(lambda_mat2)).dot(loadings2.T)
+    
+    # # find the most influential feature for anomalous SD values
+    feat_vec_sd2=which_feature_SD(df32_n.loc[sd_3.sd_mine2>SD_th2[0],:],pca_cov2)
+    sd_3['SD_feature2']=0
+    sd_3.loc[sd_3.sd_mine2>SD_th2[0],'SD_feature2']=feat_vec_sd2
+    
+    # # multiply PCA scores by the PCA loadings to get predicted X, "X-hat"
+    # # then, sbtract that from the original data to gat the PCA residuals or Orthogonal Distance, Od
+    df3_od2=(df32_n-center2)-(loadings2.dot(sc_32.T)).T
+    sd_3['od_mine2']=0
+    sd_3['od_mine2']=np.sqrt((df3_od2**2).sum(axis=1))
+    sd_3['od_flag2']=1
+    sd_3.loc[sd_3.od_mine2>OD_th2[0],'od_flag2']=0
+    # # find the most influential feature for anomalous OD values
+    feat_vec_od2=which_feature_OD(df3_od2.loc[sd_3.od_mine2>OD_th2[0],:])
+    sd_3['OD_feature2']=0
+    sd_3.loc[sd_3.od_mine2>OD_th2[0],'OD_feature2']=feat_vec_od2
+   
+    df['SD2']=sd_3.sd_mine2
+    df['SD_anomaly2']=False
+    df.loc[sd_3.sd_flag2==0,'SD_anomaly2']=True
+    df['SD_feature2']=False
+    df.loc[sd_3.SD_feature2!=0,'SD_feature2']=feat_vec_sd2
+    df['OD2']=sd_3.od_mine2
+    df['OD_anomaly2']=False
+    df.loc[sd_3.sd_flag2==0,'OD_anomaly2']=True
+    df['OD_feature2']=False
+    df.loc[sd_3.OD_feature2!=0,'OD_feature2']=feat_vec_od2
+    
 
 
 
+    msg='start single line write to mongo . Line460: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    dfh_h0=df.iloc[H0-1]
-    df_clean["mcd"]=False
-    for hh in H0:
-        df_clean.iloc[hh-1,df_clean.columns.get_loc('mcd')]=True
+    df["mcd1"]=False
+    df.loc[df_clean.iloc[H1==1].index.values,'mcd1']=True
+    
+    df["mcd2"]=False
+    df.loc[df_clean2.iloc[H12==1].index.values,'mcd2']=True
+    
     
     bin_lst=list(df._id)
-    mcd_lst=list(df2.loc[df_clean.loc[df_clean.mcd==True].index.values,'_id'])
+    #mcd_lst=list(df2.loc[df_clean.loc[df_clean.mcd==True].index.values,'_id'])
     
+    df.to_csv(str('df_'+pcap_dir+'_'+'bin_'+":"+str(index)+'.csv'))
+    collection_pcap.update_many({'_id': {'$in': bin_lst}},{'$set':{'bin':index}})
+
+    msg='start bulk write to mongo. Line477: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
+    
+    
+    from pymongo import UpdateOne
     time_bulk=timeit.default_timer()
-    bulk=collection_pcap.initialize_unordered_bulk_op()
+    #bulk=collection_pcap.initialize_unordered_bulk_op()
+    write_list=list()
     for idd in bin_lst:
-        bulk.find({'_id':idd}).update_one({'$set':{'orig_pkts_intr':df.loc[df._id==idd,'orig_pkts_intr'].values[0]
+        write_list.append(UpdateOne({'_id':idd},{'$set':{'orig_pkts_intr':df.loc[df._id==idd,'orig_pkts_intr'].values[0]
                                                     ,'orig_pkts_size':df.loc[df._id==idd,'orig_pkts_size'].values[0]
                                                     ,'serv_freq':df.loc[df._id==idd,'serv_freq'].values[0]
                                                     ,'history_freq':df.loc[df._id==idd,'history_freq'].values[0]
                                                     ,'conn_state_freq':df.loc[df._id==idd,'conn_state_freq'].values[0]
-                                                    ,'SD':df.loc[df._id==idd,'SD'].values[0]
-                                                    ,'SD_anomaly':df.loc[df._id==idd,'SD_anomaly'].values[0]
-                                                    ,'SD_feature':df.loc[df._id==idd,'SD_feature'].values[0]
-                                                    ,'OD':df.loc[df._id==idd,'OD'].values[0]
-                                                    ,'OD_anomaly':df.loc[df._id==idd,'OD_anomaly'].values[0]
-                                                    ,'OD_feature':df.loc[df._id==idd,'OD_feature'].values[0]
+                                                    ,'serv_jsd':df.loc[df._id==idd,'serv_jsd'].values[0]
+                                                    ,'history_jsd':df.loc[df._id==idd,'history_jsd'].values[0]
+                                                    ,'conn_state_jsd':df.loc[df._id==idd,'conn_state_jsd'].values[0]
+                                                   ,'mcd1':json_bool(df.loc[df._id==idd,'mcd1'].values[0])
+                                                    ,'mcd2':json_bool(df.loc[df._id==idd,'mcd2'].values[0])
+                                                    ,'SD':json_bool(df.loc[df._id==idd,'SD'].values[0])
+                                                    ,'SD_anomaly':json_bool(df.loc[df._id==idd,'SD_anomaly'].values[0])
+                                                    ,'SD_feature':json_bool(df.loc[df._id==idd,'SD_feature'].values[0])
+                                                    ,'OD':json_bool(df.loc[df._id==idd,'OD'].values[0])
+                                                    ,'OD_anomaly':json_bool(df.loc[df._id==idd,'OD_anomaly'].values[0])
+                                                    ,'OD_feature':json_bool(df.loc[df._id==idd,'OD_feature'].values[0])
+                                                    ,'SD2':json_bool(df.loc[df._id==idd,'SD2'].values[0])
+                                                    ,'SD_anomaly2':json_bool(df.loc[df._id==idd,'SD_anomaly2'].values[0])
+                                                    ,'SD_feature2':json_bool(df.loc[df._id==idd,'SD_feature2'].values[0])
+                                                    ,'OD2':json_bool(df.loc[df._id==idd,'OD2'].values[0])
+                                                    ,'OD_anomaly2':json_bool(df.loc[df._id==idd,'OD_anomaly2'].values[0])
+                                                    ,'OD_feature2':json_bool(df.loc[df._id==idd,'OD_feature2'].values[0])
                                                     }})
-    bulk.execute()   
+    )
+        if (len(write_list)%500==0):
+            collection_pcap.bulk_write(write_list,ordered=False)
+            write_list=list() 
+            msg='wrote bulk to mongo. Line513: directory= '+pcap_dir+':index='+str(index)+': write_list size:'+str(len(write_list))
+            myLogger.error(msg)
+            
+    if (len(write_list)>0):
+        collection_pcap.bulk_write(write_list,ordered=False)
+    
     elapsed_bulk=timeit.default_timer()-time_bulk
-    
-
-    
-   
-    
-    collection_pcap.update_many({'_id': {'$in': bin_lst}},{'$set':{'mcd':False}})
-    collection_pcap.update_many({'_id': {'$in': bin_lst}},{'$set':{'bin':index}})
-    collection_pcap.update_many({'_id': {'$in': mcd_lst}},{'$set':{'mcd':True}})
     
     
     llp=json.dumps(outly_pairs)
     
-    collection_bins.update_one({'pcap_dir':pcap_dir,'index':index},{'$set':{'mcd_cor':mcd_dict,'outlying_pairs':llp}},upsert=False)
+    collection_bins.update_one({'pcap_dir':pcap_dir,'index':index},{'$set':{'outlying_pairs':llp}},upsert=False)
+    msg='finished processing bin. Line513: directory= '+pcap_dir+':index='+str(index)
+    myLogger.error(msg)
     
     
 #    mcd_df2=pd.read_json(json.dumps(mcd_dict))
